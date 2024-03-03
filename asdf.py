b046ae14b090bd41f654ef96a4f4a2276d2b7b0e
Mohamed_Elsharkawy <mohamed.k.elsharkawy@gmail.com>
2024-03-03 18:55:15 +0200

    first commi

---
+++ asdf.py
@@ -0,0 +1,10 @@
+images = ['Goodbye.jpg', 'Hello.jpg', 'Help.jpg', 'Sorry.jpg', 'Thankyou.jpg']
+image_index = 0
+
+while image_index >= 0 and image_index <5:
+    
+    image_index += 1
+    print(image_index)
+        
+
+
